4697587ff6d2d2a7d745288e9849d855a83bf882
kato-zup <leonardo>
2021-01-29 14:38:05 -0300

    remove config de repos nao utilizados

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   projects: [
     "<rootDir>/apps/itau-mgm-promoter-credicard",
     "<rootDir>/apps/itau-mgm-promoter-itaucard",
-    "<rootDir>/libs/actions-manager",
   ],
 };

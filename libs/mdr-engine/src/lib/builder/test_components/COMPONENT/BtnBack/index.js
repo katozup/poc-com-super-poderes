09ae724a2bbe66a4184f8c6dd539869c1c61934f
kato-zup <leonardo>
2021-02-01 15:16:12 -0300

    adiciona store ao btnback hangler

--- libs/mdr-engine/src/lib/builder/test_components/COMPONENT/BtnBack/index.js
+++ libs/mdr-engine/src/lib/builder/test_components/COMPONENT/BtnBack/index.js
@@ -2,10 +2,11 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import './_BtnBack.scss';
+import store from '@zupmgm/mgm-redux-store';
 
 const clickHandler = (onClick) => {
   const { actionFunction, actionParameter } = onClick;
-  actionFunction(actionParameter);
+  actionFunction(actionParameter, store);
 };
 
 const BtnBack = ({ onClick, alt, iconName }) => (

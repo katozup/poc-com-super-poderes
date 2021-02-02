01a46247b946dea35b43cb45f60295457a50f1d3
kato-zup <leonardo>
2021-02-02 17:26:53 -0300

    remove libs nao existentes do jest config

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   projects: [
     "<rootDir>/apps/itau-mgm-promoter-credicard",
     "<rootDir>/apps/itau-mgm-promoter-itaucard",
-    "<rootDir>/libs/actions-manager",
     "<rootDir>/libs/mdr-engine",
-    "<rootDir>/libs/mdr-component-builder",
     "<rootDir>/libs/mgm-redux-store",
   ],
 };

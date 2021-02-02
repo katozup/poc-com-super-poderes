521e2719d49b0a16d3b20e2f54e1142d8b0a96c0
kato-zup <leonardo>
2021-02-02 17:08:22 -0300

    minor changes

--- libs/mdr-engine/src/lib/helpers/enumTranslator.js
+++ libs/mdr-engine/src/lib/helpers/enumTranslator.js
@@ -1,21 +1,14 @@
 export default function (enumValue){
   switch (enumValue){
-    case 'PAGE':
-      return 'page';
-    case 'SECTION':
-      return 'section';
-    case 'COMPONENT':
-      return 'component';
+    case 'PAGE': return 'page';
+    case 'SECTION': return 'section';
+    case 'COMPONENT': return 'component';
 
-    case 'PAGE_LOAD':
-      return 'pageLoad';
-    case 'CUSTOM_LINK':
-      return 'customLink';
+    case 'PAGE_LOAD': return 'pageLoad';
+    case 'CUSTOM_LINK': return 'customLink';
     
-    case 'ON_CLICK':
-      return 'onClick';
-    case 'ON_HOVER':
-      return 'onHover';
+    case 'ON_CLICK': return 'onClick';
+    case 'ON_HOVER': return 'onHover';
 
     default:
       return enumValue 

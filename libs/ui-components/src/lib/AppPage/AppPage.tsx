dd66b1850b06f8f3426014368432ab830c560fa6
guilherme nass <guilherme.nass@zup.com.br>
2021-02-05 08:56:58 -0300

    remoção de prop não utilizada

--- libs/ui-components/src/lib/AppPage/AppPage.tsx
+++ libs/ui-components/src/lib/AppPage/AppPage.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import './_AppPage.scss';
-import { useSelector } from 'react-redux'
 
-const AppPage = ({ children, className }) => {
-  const counter = useSelector(state => state.counter)
-
+const AppPage = ({ children }) => {
   return (
     <div className='app-wrapper'>
       { children }

717dce4e4d2b0cddb5ae25d90c1c2973fb68be86
kato-zup <leonardo>
2021-02-05 09:34:01 -0300

    precommit de funcionalidades e sagas de acoes

--- libs/mdr-engine/src/lib/builder/functions/sdkFunctions/closeWebview.js
+++ libs/mdr-engine/src/lib/builder/functions/sdkFunctions/closeWebview.js
@@ -1,4 +1,13 @@
-export default function (param) {
-  console.log('closing: ',param);
-  return param;
+export default function () {
+  try {
+    const { native } = window;
+    const jsonProtocol = {};
+    jsonProtocol.action = 'com.itau.CLOSE_WINDOW';
+    jsonProtocol.category = 'com.itau.category.DEFAULT';
+
+    const jsonString = JSON.stringify(jsonProtocol);
+    return native.requestNativeFeature(jsonString, 'responseNativeFeature');
+  } catch (error) {
+    console.log(error);
+  }
 }
--- libs/mdr-engine/src/lib/builder/functions/sdkFunctions/shareLink.js
+++ libs/mdr-engine/src/lib/builder/functions/sdkFunctions/shareLink.js
@@ -1,4 +1,23 @@
-export default function (param) {
-  console.log('sharing: ',param);
-  return param;
-}
+// TODO implementar o saga com a chamada do endpoint de geracao de link antes de finalizar a task
+
+const sharingLinkSDK = (shareContent, shareType) => {
+  const { native } = window;
+  const jsonProtocol = {};
+
+  switch (shareType) {
+    case 'whatsApp':
+      jsonProtocol.action = 'com.itau.WHATSAPP_SHARING';
+      break;
+    default:
+      jsonProtocol.action = 'com.itau.OTHER_APP_SHARING';
+      break;
+  }
+
+  jsonProtocol.category = 'com.itau.category.DEFAULT';
+  jsonProtocol.data = `${shareContent.message}`;
+
+  const jsonString = JSON.stringify(jsonProtocol);
+  native.requestNativeFeature(jsonString, 'responseNativeFeature');
+};
+
+export default sharingLinkSDK;

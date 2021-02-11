8ff39d847fa2e06027dbb0e3e6c188a2b1e1cf98
kato-zup <leonardo>
2021-02-11 15:34:43 -0300

    adiciona caller da action de init no appjs do subrepo prom:cred

--- libs/mgm-redux-store/src/index.js
+++ libs/mgm-redux-store/src/index.js
@@ -4,3 +4,4 @@ export default store;
 export { Creators as counterActions } from './lib/ducks/counter';
 export { Creators as analyticsActions } from './lib/ducks/analytics';
 export { Creators as shareActions } from './lib/ducks/share';
+export { Creators as appActions } from './lib/ducks/app';
--- libs/mgm-redux-store/src/lib/sagas/appInit.js
+++ libs/mgm-redux-store/src/lib/sagas/appInit.js
@@ -1,24 +1,21 @@
-import { call, put } from 'redux-saga/effects';
+import { put } from 'redux-saga/effects';
 
-import { ERROR_TYPES } from '../../constants';
+// import { ERROR_TYPES } from '../../constants';
 import { Creators as AppActions } from '../ducks/app';
-import { Creators as ErrorActions } from '../ducks/error';
+// import { Creators as ErrorActions } from '../ducks/error';
 // import setAnalyticsInformation from './analytics';
 // import authentication from './authentication';
 // import { getContent } from './content';
 // import { createCustomer } from './customer';
 
-const {
-  FLOW: { INIT_APP },
-} = ERROR_TYPES;
-const { cleanErrorConditionsAndRetryCounts, callErrorHandler } = ErrorActions;
 const { stopLoading } = AppActions;
 
-export function* initApp() {
+export function* appInit() {
+  // TODO remover console abaixo apos implementar o saga de init
+  console.log('Saga do App init')
   try {
     // pegar dados do user (mockado e sdk)
     // autenticar
-    // bater no payload do sdui e salvar conteudo (ver se vale a pena deixar aqui ou colocar no index do app memo)
     // bater no criar customer
     // bater no payload de analytics (implementar na task seguinte)
     return yield put(stopLoading());

83438627d13c10f6263e248a17500b0a506fd9fb
kato-zup <leonardo>
2021-02-11 11:57:09 -0300

    pre commit do saga de sharing + setup de const dos endpoints

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,1 +1,3 @@
 export * from "./lib/parse-sanitized-html/parse-sanitized-html";
+export * from "./lib/applicationActions/endpoints/getShareLink/getShareLink";
+export * from "./lib/applicationActions/sdk/shareLink/shareLink";

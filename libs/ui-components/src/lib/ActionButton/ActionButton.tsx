eea95618d68d517485e8ea113a8e4e7cf3155b89
guilherme nass <guilherme.nass@zup.com.br>
2021-02-26 09:56:47 -0300

    49887 - inclusão de comportamento de loading nos botões principais da tela

--- libs/ui-components/src/lib/ActionButton/ActionButton.tsx
+++ libs/ui-components/src/lib/ActionButton/ActionButton.tsx
@@ -1,24 +1,48 @@
 /* eslint-disable jsx-a11y/no-autofocus */
-import React from 'react'
 import PropTypes from 'prop-types';
+import React from 'react';
+import { RootStateOrAny, useSelector } from 'react-redux';
+import ButtonLoading from '../ButtonLoading/ButtonLoading';
 import './_ActionButton.scss';
 
-const clickHandler = (onClick) => {
-  const { actionFunction, actionParameter } = onClick;
-  actionFunction(actionParameter);
+const clickHandler = (onClick, styling, componentId) => {
+  const { actionFunction } = onClick;
+  const buttonIndex = styling === 'primary' ? 0 : 1;
+  const type = styling === 'primary' ? 'whatsApp' : 'otherApps';
+  actionFunction(type, buttonIndex, componentId);
 };
 
-const ActionButton = ({ text, onClick, alt, styling }) => (
+const ActionButton = ({ text, onClick, alt, styling, componentId }) => (
   <button
-    autoFocus
+    id={componentId}
+    disabled={isButtonDisabled()}
     aria-label={alt}
-    onClick={() => clickHandler(onClick)}
-    type="button"
+    onClick={() => clickHandler(onClick, styling, componentId)}
+    type='button'
     className={`action-button ${getButtonStyle(styling)}`}
   >
-    <span className="icon">{text}</span>
+    {isButtonLoading(componentId) ? (
+      <ButtonLoading loadPrimary={styling === 'primary'} />
+    ) : (
+      text
+    )}
   </button>
 );
+
+const isButtonLoading = (componentId) => {
+  const shareButton = useSelector((state: RootStateOrAny) => state.share);
+  if (Object.keys(shareButton).length > 0) {
+    return shareButton[componentId] && shareButton[componentId].isLoading;
+  }
+  return false;
+};
+
+const isButtonDisabled = () => {
+  const shareButton = useSelector((state: RootStateOrAny) => state.share);
+  return Object.values(shareButton).some(
+    (button: any) => button.isLoading === true
+  );
+};
 
 export function getButtonStyle(styling) {
   return `${styling}-button`;

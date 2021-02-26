77d141603ed362248e4000b9001e2b1c09ef2053
guilherme nass <guilherme.nass@zup.com.br>
2021-02-26 09:41:02 -0300

    49887 - ajuste reducer para controle de loading dos botões

--- libs/mgm-redux-store/src/lib/ducks/share.js
+++ libs/mgm-redux-store/src/lib/ducks/share.js
@@ -3,26 +3,20 @@ export const Types = {
   SHARE_SUCCESS: 'share/SUCCESS',
 };
 
-const INITIAL_STATE = {
-  button: [{ loading: false }, { loading: false }],
-};
+const INITIAL_STATE = {};
 
 export default function shareReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case Types.SHARE_REQUEST:
       return {
         ...state,
-        button: state.button.map((_, index) =>
-          index === action.payload.buttonIndex
-            ? { loading: true }
-            : { loading: false }
-        ),
+        [action.payload.componentId]: { isLoading: true },
       };
 
     case Types.SHARE_SUCCESS:
       return {
         ...state,
-        button: state.button.map(() => false),
+        [action.payload.componentId]: { isLoading: false },
       };
 
     default:
@@ -31,12 +25,13 @@ export default function shareReducer(state = INITIAL_STATE, action) {
 }
 
 export const Creators = {
-  shareRequest: (type, buttonIndex, gatewayAppKey) => ({
+  shareRequest: (type, buttonIndex, componentId) => ({
     type: Types.SHARE_REQUEST,
-    payload: { type, buttonIndex, gatewayAppKey },
+    payload: { type, buttonIndex, componentId },
   }),
 
-  shareSuccess: () => ({
+  shareSuccess: (componentId) => ({
     type: Types.SHARE_SUCCESS,
+    payload: { componentId },
   }),
 };

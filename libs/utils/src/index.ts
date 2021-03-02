5060d1b6cf13ee232c0a2e8008940cf8008fe045
guilherme nass <guilherme.nass@zup.com.br>
2021-03-02 19:44:43 -0300

    53993 - ajuste nome export

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -3,9 +3,10 @@ export * from './lib/applicationActions/endpoints/getShareLink';
 export * from './lib/applicationActions/endpoints/getSduiContent';
 export * from './lib/applicationActions/endpoints/getDnDefault';
 export * from './lib/applicationActions/sdk/shareLink/shareLink';
-export * from './lib/sdk/index';
+export * from './lib/sdk/getSdkData';
 export * from './lib/constants/SHARED';
 export * from './lib/constants/ERROR_TYPES';
 export * from './lib/constants/index';
 export * from './lib/config/environment';
 export * from './lib/applicationActions/endpoints/login';
+

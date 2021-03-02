48cb3a5a264f93e3a65b57ca0fc0fc0ea057c2dd
guilherme nass <guilherme.nass@zup.com.br>
2021-03-02 19:43:28 -0300

    53993 - inclusão de chamadas de reducers e sagas de erro

--- libs/mgm-redux-store/src/lib/sagas/index.js
+++ libs/mgm-redux-store/src/lib/sagas/index.js
@@ -1,12 +1,17 @@
 import { all, takeLatest } from 'redux-saga/effects';
 import { Types as AppTypes } from '../ducks/app';
 import { Types as ShareTypes } from '../ducks/share';
+import { Types as ErrorTypes } from '../ducks/error';
 import { initApp } from './initApp';
 import { getLinkAndShare } from './share';
+import errorHandler from './errors/handlers/errorHandler';
+import tryAgainHandler from './errors/handlers/tryAgainHandler';
 
 export default function* rootSaga() {
   yield all([
     takeLatest(AppTypes.INIT_APP, initApp),
     takeLatest(ShareTypes.SHARE_REQUEST, getLinkAndShare),
+    takeLatest(ErrorTypes.CALL_ERROR_HANDLER, errorHandler),
+    takeLatest(ErrorTypes.TRY_AGAIN, tryAgainHandler)
   ]);
 }

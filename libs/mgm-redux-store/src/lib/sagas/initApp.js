fdf3a68991d6745ae0fc9aaaca8b34183864dfdd
guilherme nass <guilherme.nass@zup.com.br>
2021-03-03 14:00:54 -0300

    53993 - disparo de reducer de erro

--- libs/mgm-redux-store/src/lib/sagas/initApp.js
+++ libs/mgm-redux-store/src/lib/sagas/initApp.js
@@ -1,12 +1,18 @@
-import { call, put, select } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 import sduiContent from './sduiContent';
 import setSdkData from './sdkData';
-import authentication from './authentication'
+import authentication from './authentication';
+import { ERROR_TYPES } from '@zup-mgm/utils';
 import { createCustomer } from './customer';
 import { Creators as AppActions } from '../ducks/app';
+import { Creators as ErrorActions } from '../ducks/error';
 import setAnalyticsInformation from './analytics';
 
 const { stopLoading } = AppActions;
+const {
+  FLOW: { INIT_APP },
+} = ERROR_TYPES;
+const { cleanErrorConditionsAndRetryCounts, callErrorHandler } = ErrorActions;
 
 export function* initApp() {
   try {
@@ -15,9 +21,9 @@ export function* initApp() {
     yield call(sduiContent);
     yield call(createCustomer);
     yield call(setAnalyticsInformation);
+    yield put(cleanErrorConditionsAndRetryCounts());
     return yield put(stopLoading());
   } catch (error) {
-    //TODO: Chamar saga de erro após implementar
-    console.log(error);
+    return yield put(callErrorHandler(error, INIT_APP));
   }
 }

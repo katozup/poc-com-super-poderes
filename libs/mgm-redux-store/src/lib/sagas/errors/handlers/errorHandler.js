6b5b93c5d4a66c22e0feb3c7646f2c0ca5d6ebbb
guilherme nass <guilherme.nass@zup.com.br>
2021-03-04 19:13:20 -0300

    53993 - refatoração e remoção de condições não necessárias

--- libs/mgm-redux-store/src/lib/sagas/errors/handlers/errorHandler.js
+++ libs/mgm-redux-store/src/lib/sagas/errors/handlers/errorHandler.js
@@ -1,7 +1,7 @@
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
 import * as Sentry from '@sentry/browser';
 import { call, select } from 'redux-saga/effects';
-import { environment } from '@zup-mgm/utils';
-import { BUSINESS_RULES, HTTP_STATUS, ERROR_TYPES } from '@zup-mgm/utils';
+import { BUSINESS_RULES, HTTP_STATUS, ERROR_TYPES, environment } from '@zup-mgm/utils';
 import extractErrorStatus from '../extractors/extractErrorStatus';
 import analyticsErrorHandler from './analyticsErrorHandler';
 import genericErrorHandler from './genericErrorHandler';
@@ -12,35 +12,44 @@ const { CRASH_REPORT_ON } = environment;
 const { MAX_AUTOMATIC_RETRY } = BUSINESS_RULES;
 const { UNAUTHORIZED } = HTTP_STATUS;
 const {
-  ANALYTICS: { ITAU_TRACK, ZUP_TRACK },
-  SDK: { CLOSE_WEBVIEW, GET_SDK_ITEM, GET_COUPON },
+  ANALYTICS: { ITAU_TRACK },
+  SDK: { CLOSE_WEBVIEW, GET_SDK_ITEM },
 } = ERROR_TYPES;
 
 export default function* errorHandler(action) {
   const { error, whereErrorOccurred } = action.payload;
-  const errorStatus = extractErrorStatus(error);
-  const { automaticRetryCount } = yield select((state) => state.error);
-  const isUnauthorizedError = errorStatus === UNAUTHORIZED;
-  const canAutomaticRetry = automaticRetryCount < MAX_AUTOMATIC_RETRY;
-  const isAnalyticsError =
-    whereErrorOccurred === ITAU_TRACK || whereErrorOccurred === ZUP_TRACK;
-  const isSdkError =
-    whereErrorOccurred === CLOSE_WEBVIEW ||
-    whereErrorOccurred === GET_SDK_ITEM ||
-    whereErrorOccurred === GET_COUPON;
 
   if (CRASH_REPORT_ON) Sentry.captureException(error);
 
-  if (isAnalyticsError) {
+  if (isAnalyticsError(whereErrorOccurred)) {
     return yield call(analyticsErrorHandler, whereErrorOccurred);
   }
 
-  if (isSdkError) {
+  if (isSdkError(whereErrorOccurred)) {
     return yield call(sdkErrorHandler, whereErrorOccurred);
   }
 
-  if (isUnauthorizedError && canAutomaticRetry) {
+  if (isUnauthorizedError(error) && canAutomaticRetry()) {
     return yield call(unauthorizedErrorHandler, error, whereErrorOccurred);
   }
   return yield call(genericErrorHandler, error, whereErrorOccurred);
 }
+
+const isAnalyticsError = (whereErrorOccurred) => {
+  return whereErrorOccurred === ITAU_TRACK;
+}
+
+const isSdkError = (whereErrorOccurred) => {
+  return whereErrorOccurred === CLOSE_WEBVIEW || whereErrorOccurred === GET_SDK_ITEM;
+}
+
+const isUnauthorizedError = (error) => {
+  const errorStatus = extractErrorStatus(error);
+  return errorStatus === UNAUTHORIZED;
+};
+
+function* canAutomaticRetry() {
+  const { automaticRetryCount } = yield select(state => state.error);
+  return automaticRetryCount < MAX_AUTOMATIC_RETRY;
+}
+

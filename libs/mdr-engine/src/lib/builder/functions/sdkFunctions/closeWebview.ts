358094ddac2aacbc2a3344dca3f736c71ec550c7
guilherme nass <guilherme.nass@zup.com.br>
2021-03-04 19:35:31 -0300

    53993 - dispatch de erro ao tentar fechar webview

--- libs/mdr-engine/src/lib/builder/functions/sdkFunctions/closeWebview.ts
+++ libs/mdr-engine/src/lib/builder/functions/sdkFunctions/closeWebview.ts
@@ -1,26 +1,36 @@
+import { store, errorActions } from '@zup-mgm/mgm-redux-store';
+import { ERROR_TYPES } from '@zup-mgm/utils';
+
 declare global {
   interface Window {
     native: {
-      requestNativeFeature: (arg1: string, arg2: string) => void,
+      requestNativeFeature: (arg1: string, arg2: string) => void;
     };
   }
 }
 
-export default function () {
+const {
+  SDK: { CLOSE_WEBVIEW },
+} = ERROR_TYPES;
+const { callErrorHandler } = errorActions;
+
+const closeWebview = () => {
   try {
     console.log('Vai tentar chamar o SDK nativo para fechar a webview');
     const { native } = window;
     const jsonProtocol: {
-      action: string,
-      category: string,
+      action: string;
+      category: string;
     } = {
       action: 'com.itau.CLOSE_WINDOW',
       category: 'com.itau.category.DEFAULT',
     };
-    
+
     const jsonString = JSON.stringify(jsonProtocol);
     return native.requestNativeFeature(jsonString, 'responseNativeFeature');
   } catch (error) {
-    console.log(error);
+    return store.dispatch(callErrorHandler(error, CLOSE_WEBVIEW));
   }
-}
+};
+
+export default closeWebview;

b5896bbaba8019e3f8aa956d89f3b690e965635a
guilherme nass <guilherme.nass@zup.com.br>
2021-03-04 15:40:09 -0300

    49887 - ajustes arquivos de mock

---
+++ libs/utils/src/lib/applicationActions/sdk/getSdkData.js
@@ -0,0 +1,60 @@
+import { sdkDataMock } from '@zup-mgm/mock';
+import getFirstName from './getFirstName';
+import getSDKItem from './getSDKItem';
+
+const getSdkData = async () => {
+  const params = new URLSearchParams(window.location.search);
+  const sdkParam = params.get('sdk');
+
+  if (sdkParam === false || sdkParam === 'false') {
+    const userData = sdkDataMock();
+    userData.name = getFirstName(userData.name);
+
+    const cardTypeParam = params.get('app');
+    userData.cardType = cardTypeParam;
+
+    return userData;
+  }
+
+  const cpf = await getSDKItem('CUSTOMER_CPF');
+  const autenticacao = await getSDKItem('MGM_AUTHENTICATION');
+  const customerId = await getSDKItem('CUSTOMER_ID');
+  const cardVariant = await getSDKItem('CARD_VARIANT');
+  const cardName = await getSDKItem('CARD_NAME');
+  const cardFlag = await getSDKItem('CARD_FLAG');
+  const dn = await getSDKItem('DN');
+  const name = getFirstName(await getSDKItem('CUSTOMER_NAME'));
+  const chpras = await getSDKItem('CHPRAS');
+  const cpfHashed = await getSDKItem('CPF_HASHED');
+  const customerType = await getSDKItem('CUSTOMER_TYPE');
+  let featureCashback = await getSDKItem('CASHBACK');
+  const flowByHipercardApp = await getSDKItem('HIPERCARD');
+  const flowByCartaoLuizaApp = await getSDKItem('CARTAOLUIZA');
+
+  if (featureCashback == null) {
+    featureCashback = false;
+  }
+
+  const lastDigits = await getSDKItem('LAST_FOUR_CHARACTERS');
+  const userData = {
+    cpf,
+    autenticacao,
+    chpras,
+    dn,
+    name,
+    customerId,
+    cardVariant,
+    cardName,
+    cardFlag,
+    lastDigits,
+    featureCashback,
+    cpfHashed,
+    customerType,
+    flowByHipercardApp,
+    flowByCartaoLuizaApp,
+  };
+
+  return userData;
+};
+
+export default getSdkData;

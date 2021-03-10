501ed5eca64eb47e60e3a5662cbde17cc59d6e16
guilherme nass <guilherme.nass@zup.com.br>
2021-03-10 15:26:34 -0300

    49829 - mock do useSelector para teste unitário

--- libs/ui-components/src/lib/DefaultError/default-error.spec.tsx
+++ libs/ui-components/src/lib/DefaultError/default-error.spec.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
+import * as reactRedux from 'react-redux';
 
-import DefaultError from "./DefaultError";
+import DefaultError from './DefaultError';
 
-describe("DefaultError", () => {
-  it("should render successfully", () => {
+describe('DefaultError', () => {
+  const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+  it('should render successfully', () => {
+    useSelectorMock.mockReturnValue({});
     const { baseElement } = render(<DefaultError />);
     expect(baseElement).toBeTruthy();
   });

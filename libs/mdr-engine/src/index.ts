0f8b4b4deb30ab02e7b225235e4f6d2d98750ed8
Guilherme Nass <nassguilherme@gmail.com>
2021-03-18 15:11:03 -0300

    50526 - remoção de import desnecessário

--- libs/mdr-engine/src/index.ts
+++ libs/mdr-engine/src/index.ts
@@ -1,3 +1,2 @@
 export * from './lib/mdr-engine';
 export { default as closeWebview } from './lib/builder/functions/sdkFunctions/closeWebview';
-export { ActionName } from './lib/helpers/enums/actionName';

8506cdf25850537415f460c0b4a130f18be45314
Guilherme Nass <nassguilherme@gmail.com>
2021-03-19 10:56:31 -0300

    50526 - controle de loading no componente principal

--- libs/ui-components/src/lib/AppPage/AppPage.tsx
+++ libs/ui-components/src/lib/AppPage/AppPage.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { RootStateOrAny, useSelector } from 'react-redux';
+import Loading from '../Loading/Loading';
 import './_AppPage.scss';
 
 const AppPage = ({ children, analytics, backgroundImage }) => {
+  const loading = useSelector((state: RootStateOrAny) => state.app.loading);
   const trackPageLoad = (analytics) => {
     if (analytics) {
       analytics.analyticsFunction(analytics.analyticsParameter);
@@ -25,7 +27,11 @@ const AppPage = ({ children, analytics, backgroundImage }) => {
       </div>
     );
   }
-  
+
+  if (loading) {
+    return <Loading loadPrimary={false} />;
+  }
+
   return (
     <div>
       <div className='app-wrapper'>{children}</div>

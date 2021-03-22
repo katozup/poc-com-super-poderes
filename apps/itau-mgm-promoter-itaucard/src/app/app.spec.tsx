c1fe37a98615077426863b7f6802e5a2cba51c82
guilhermenass <guilherme.nass@zup.com.br>
2021-03-22 17:15:14 -0300

    50526 - ajuste testes unitários

--- apps/itau-mgm-promoter-itaucard/src/app/app.spec.tsx
+++ apps/itau-mgm-promoter-itaucard/src/app/app.spec.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { render } from "@testing-library/react";
-
+import * as reactRedux from 'react-redux'
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./app";
 
 describe("App", () => {
+
+  const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+  const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
+  const mockedDispatch = jest.fn();
+
+  beforeEach(() => {
+    useSelectorMock.mockClear();
+    useDispatchMock.mockClear();
+  })
   it("should render successfully", () => {
+    useSelectorMock.mockReturnValue({});
+    useDispatchMock.mockReturnValue(mockedDispatch);
     const { baseElement } = render(
       <BrowserRouter>
         <App />
@@ -14,15 +25,5 @@ describe("App", () => {
     );
 
     expect(baseElement).toBeTruthy();
-  });
-
-  it("should have a greeting as the title", () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-
-    expect(getByText("Welcome to itau-mgm-promoter-itaucard!")).toBeTruthy();
   });
 });
